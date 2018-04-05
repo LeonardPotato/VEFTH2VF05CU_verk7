908ba204d0235eda4d9d2a3d1d29ba0b9f03136e
LeonardPotato <eggertaron@gmail.com>
2018-04-05 15:49:33 +0000

    Add files via upload

--- verk7.py
+++ verk7.py
@@ -1,5 +1,6 @@
-from bottle import run, route, static_file, template, request, response, redirect
+from bottle import run, route, static_file, template, request, response, redirect, app
 import os
+from beaker.middleware import SessionMiddleware
 
 adminuser = 'admin'
 adminpwd = '12345'
@@ -43,5 +44,57 @@ def signout():
     return "You have been signed out." \
             "<br> <a href='/login'>Log in</a>"
 
-run()
-#run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#Session lausn
+
+session_options = {
+    'session.type': 'file',
+    'session.data_dir': './data'
+}
+
+my_session = SessionMiddleware(app(), session_options)
+
+products = [
+    {"pid": 1, "name": "Vara A", "price": 100},
+    {"pid": 2, "name": "Vara B", "price": 400},
+    {"pid": 3, "name": "Vara C", "price": 800},
+    {"pid": 4, "name": "Vara D", "price": 300}
+]
+
+@route('/shop')
+def shop():
+    return template('shop', products=products)
+
+@route('/cart/add/<id>')
+def add_to_cart(id):
+    session = request.environ.get('beaker.session')
+    session[id] = products[int(id)-1]['name']
+    session.save()
+
+    print(session)
+    return redirect('/cart')
+
+@route('/cart')
+def cart():
+    session = request.environ.get('beaker.session')
+    karfa = []
+    for i in range(1, len(products)+1):
+        i = str(i)
+        if session.get(i):
+            vara = session.get(i)
+            karfa.append(vara)
+
+    return template('cart', karfa=karfa)
+
+@route('/cart/remove')
+def remove_cart():
+    session = request.environ.get('beaker.session') # ??
+    session.delete()    # deletear sessioninu
+    return redirect('/shop') #redirectar user a /shop
+
+
+
+
+
+
+run(app = my_session)
+#run(app = my_session, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
